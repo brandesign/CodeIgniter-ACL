b75d35ad6ad422107b06cb43da3d27ff59c411e7
David Brandes <david.brandes@gmail.com>
2012-09-14 13:21:25 +0200

    Fix missing '

--- application/language/english/acl_auth_lang.php
+++ application/language/english/acl_auth_lang.php
@@ -4,4 +4,4 @@ $lang['register_failed']			= 'Register failed';
 $lang['login_failed']				= 'Login failed';
 $lang['reset_user_not_found']		= 'User not found';
 $lang['reset_token_expired']		= 'Reset token expired';
-$lang['reset_token_check_failed']	= 'Reset token doesn\'t match;
+$lang['reset_token_check_failed']	= 'Reset token doesn\'t match';
