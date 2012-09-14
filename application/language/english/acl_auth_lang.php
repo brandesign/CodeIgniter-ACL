53a8cb02fd49febbad3533507d4b0d3e3c155ae2
David Brandes <david.brandes@gmail.com>
2012-09-14 13:20:03 +0200

    Forgot language files in last commit
    Sorry, the language files are quite ugly. Need to manage them better.

---
+++ application/language/english/acl_auth_lang.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['register_failed']			= 'Register failed';
+$lang['login_failed']				= 'Login failed';
+$lang['reset_user_not_found']		= 'User not found';
+$lang['reset_token_expired']		= 'Reset token expired';
+$lang['reset_token_check_failed']	= 'Reset token doesn\'t match;
---
+++ application/language/german/acl_auth_lang.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['register_failed']			= 'Registrierung fehlgeschlagen';
+$lang['login_failed']				= 'Login fehlgeschlagen';
+$lang['reset_user_not_found']		= 'Benutzer nicht gefunden';
+$lang['reset_token_expired']		= 'Der Reset Code ist nicht mehr gültig';
+$lang['reset_token_check_failed']	= 'Der Reset Code ist falsch';
